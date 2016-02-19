0ec289b05478d1427ff652bf1d9f7a5ad78a52c3
Alejandro Rodriguez <alex.mognom@gmail.com>
2016-02-19 14:04:06 +0000

    Preferences

--- jira-harvester-operator/src/js/main.js
+++ jira-harvester-operator/src/js/main.js
@@ -21,57 +21,47 @@
     var oauth2Token;
 
     var projectId = "APP";
-    var rapidViewId = "1";
 
     var component = "Wirecloud";
 
-
-
-    ///rest/api/2/search?jql=project="Your Project Key"
-
     var init = function init() {
+
+        MashupPlatform.wiring.registerStatusCallback (pushInfo);
+
         //On preferences update
-        MashupPlatform.prefs.registerCallback(function (new_preferences) {
-            //Updates the OAUTH token
-            
-            if ("oauth2-token" in new_preferences) {
-                oauth2Token = MashupPlatform.prefs.get("oauth2-token");
-            }
+        MashupPlatform.prefs.registerCallback(updatePrefs);
+        updatePrefs();
+    };
 
-            //Updates the Login credentials
-            if ("username" in new_preferences || "passwd" in new_preferences) {
-                //token = atob(MashupPlatform.prefs.get("username") + ":" + MashupPlatform.prefs.get("passwd"));
-                //requestHeaders.Authorization = token;
-            }
+    var updatePrefs = function updatePrefs() {
+        //Updates the OAUTH token
+        oauth2Token = MashupPlatform.prefs.get("oauth2-token");
 
-            //Updates the jira instance URI
-            if ("jira-url" in new_preferences) {
-                //baseURI = MashupPlatform.prefs.get("jira-url");
-            }
+        //Updates the Login credentials
+        token = btoa(MashupPlatform.prefs.get("username") + ":" + MashupPlatform.prefs.get("passwd"));
+        requestHeaders.Authorization = token;
 
-            //Updates the target issue
-            if ("project-key" in new_preferences) {
-                projectId = MashupPlatform.prefs.get("project-key");
-            }
+        //Updates the jira instance URI
+        baseURI = MashupPlatform.prefs.get("jira-url");
 
-            if ("rapid-view" in new_preferences) {
-                rapidViewId = MashupPlatform.prefs.get("rapid-view");
-            }
-            
-            pushInfo();
-        });
+        //Updates the target project
+        projectId = MashupPlatform.prefs.get("project-key");
+
+        //Updates the target component
+        component = MashupPlatform.prefs.get("component");
+        pushInfo();
     };
 
     //Sends events through the connected outputs
     var pushInfo = function pushInfo() {
-
         //Gets version info and all component issues
         requestProjectVersions();
     };
 
+    // Request all the versions of the project (sprints)
     var requestProjectVersions = function requestProjectVersions() {
         // http://jira.fiware.org/rest/api/2/project/APP/versions
-        MashupPlatform.http.makeRequest (baseURI +"/rest/api/2/project/" + projectId + "/versions", {
+        MashupPlatform.http.makeRequest (baseURI + "/rest/api/2/project/" + projectId + "/versions", {
             method: 'GET',
             supportsAccessControl: false,
             requestHeaders: requestHeaders,
@@ -80,40 +70,40 @@
 
                 requestComponentIssues(data);
             }
-        });        
+        });
     };
 
+    // Request all the issues associated to the component
     var requestComponentIssues = function requestComponentIssues(versions) {
         //http://jira.fiware.org/rest/api/2/search?jql=component%3DWirecloud&maxResults=1000
 
-        MashupPlatform.http.makeRequest (baseURI +"/rest/api/latest/search?jql=component%3D" + component + "&maxResults=1000", {
+        MashupPlatform.http.makeRequest (baseURI + "/rest/api/latest/search?jql=component%3D" + component + "&maxResults=1000", {
             method: 'GET',
             supportsAccessControl: false,
 
             requestHeaders: requestHeaders,
             onSuccess: function (response) {
                 var data = JSON.parse(response.responseText);
-                
                 var msg = data.issues;
-
                 for (var i = 0; i < msg.length; i++) {
                     // Makes version info consistent
                     msg[i].fields.version = {};
                     msg[i].fields.version.name = "";
-                    if(msg[i].fields.fixVersions && msg[i].fields.fixVersions[0]) {
+                    if (msg[i].fields.fixVersions && msg[i].fields.fixVersions[0]) {
                         msg[i].fields.version = msg[i].fields.fixVersions[0];
                     } else if (msg[i].fields.versions && msg[i].fields.versions[0]) {
-                        msg[i].fields.version.name = msg[i].fields.versions[0];
+                        msg[i].fields.version = msg[i].fields.versions[0];
                     }
 
                     //Add some metadata
-                    msg.metadata= {};
+                    msg.metadata = {};
                     msg.metadata.versions = versions; // :)
                     msg.metadata.type = "list";
                     msg.metadata.tag = "Issue";
                     msg.metadata.verbose = "Jira issues";
                 }
 
+                //Pushes the list of issues
                 MashupPlatform.wiring.pushEvent("jira-issues", msg);
             }
         });
