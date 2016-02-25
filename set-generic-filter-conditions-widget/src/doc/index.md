6fd5887ab49f04561bac63312eba1851c503213b
Alejandro Rodriguez <alex.mognom@gmail.com>
2016-02-25 15:44:03 +0100

    selector creation

---
+++ set-generic-filter-conditions-widget/src/doc/index.md
@@ -0,0 +1,17 @@
+## Introduction
+
+Filter data
+
+## Settings
+
+## Wiring
+
+### Input Endpoints
+
+### Output Endpoints
+
+## Usage
+
+## Reference
+
+- [FIWARE Mashup](https://mashup.lab.fiware.org/)
---
+++ set-generic-filter-conditions-widget/src/js/main.js
@@ -0,0 +1,105 @@
+
+/*
+ * set-generic-filter-conditions-widget
+ * https://repo.conwet.fi.upm.es/wirecloud/agile-dashboards
+ *
+ * Copyright (c) 2016 CoNWet
+ * Licensed under the MIT license.
+ */
+
+(function () {
+
+    "use strict";
+
+    var data = null;
+    var selectors = [];
+
+    var init = function init() {
+        MashupPlatform.wiring.registerCallback("input", function (d) {
+            data = d;
+            calculateFilters();
+        });
+    };
+
+    var calculateFilters = function calculateFilters () {
+        //Wait for data
+        if(data === null) {
+            return;
+        }
+        //Check if data has filter info
+        if(!data.metadata || data.metadata.filters || data.metadata.filters.length > 0) {
+            return; //No filters available
+        }
+        //Clear previous filters
+        clearSelectors();
+
+        var dict = data.metadata.filters;
+        dict.forEach(function (filter) {
+            createSelector(filter);
+        });
+    };
+
+    //Removes current selectors
+    var clearSelectors = function clearSelectors () {
+        selectors = [];
+
+        //Remove them from the view
+        var body = document.getElementById('filters');
+        body.innerHTML = "";
+
+    };
+
+    var getProperty = function getProperty (item, attr) {
+        attr = attr.split(".");
+        for (var i = 0; i < attr.length; i++) {
+            item = item[attr[i]] ;
+        }
+        return item;
+    };
+
+    //Creates a new selector based on the input filter
+    var createSelector = function createSelector (filter) {
+        //Clear body
+        var body = document.getElementById('filters');
+        var div = document.createElement('div');
+        var title = new StyledElements.Fragment("<h4>" + filter.name + "</h4>");
+        var select = new StyledElements.Select();
+        var found = [];
+        var entries = [];
+
+        entries.push({label: 'All', value: ''});
+        data.forEach( function (item) {
+            var prop = getProperty(item, filter.property);
+            if (!found[prop]) {
+                found[prop] = true;
+                entries.push({label: prop, value: prop});
+            }
+            //found[issue.fields.assignee.name] = true;
+            //entries.push({label: issue.fields.assignee.displayName, value: issue.fields.assignee.name});
+        });
+
+        select.addEntries(entries);
+        select.addEventListener("change", sendEvents.bind(this));
+        //versionSelect.addEventListener('change', sendEvents.bind(this));
+        //versionSelect.insertInto(document.body);
+        
+        //Add the selector to the view
+        div.appendChild(title);
+        div.appendChild(select);
+        body.appendChild(div);
+
+        //Save the new selector
+        selectors.push(select);
+    };
+
+    var sendEvents = function sendEvents() {
+
+    };
+
+    //Init the widget
+    init();
+    /* test-code */
+
+    /* end-test-code */
+
+})();
