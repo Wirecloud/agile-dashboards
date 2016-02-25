403d9fa26e71c701653b4179ba751362f261287f
Alejandro Rodriguez <alex.mognom@gmail.com>
2016-02-25 12:36:50 +0100

    Same sprint check

--- burndown-chart-generator-operator/src/js/main.js
+++ burndown-chart-generator-operator/src/js/main.js
@@ -10,14 +10,38 @@
 
     "use strict";
 
-    var dayLength = 86400000;
-
+    var DAY_LENGTH = 86400000;
     var sprint = null;
-
     var startDate = null;
     var endDate = null;
-
     var version = null;
+
+    var init = function init() {
+        MashupPlatform.wiring.registerCallback("issues", function (data) {
+            sprint = data;
+            version = null;
+            if (sprint.length === 0) {
+                cleanPlot("Input data is empty :(");
+                return;
+            }
+
+            //Check that all issues are from the same sprint/release
+            version = sprint[0].fields.version.name;
+            if (!checkOneSprint()) {
+                cleanPlot("All input issues must be from the same sprint");
+                return;
+            }
+
+            data.metadata.versions.forEach(function (ver) {
+                if (ver.name === version) {
+                    startDate = ver.startDate;
+                    endDate = ver.releaseDate;
+                }
+            });
+
+            plot();
+        });
+    };
 
     // Calculates the reference data series
     var calculateInterval = function calculateInterval(days, issuesCount) {
@@ -47,7 +71,7 @@
             dailyCount = 0;
             for (var i = 0; i < issues.length; i++) {
 
-                if (day >= Date.parse(issues[i].fields.resolutiondate) - dayLength + 1) {
+                if (day >= Date.parse(issues[i].fields.resolutiondate) - DAY_LENGTH + 1) {
                     dailyCount++;
                     issues.splice(i, 1); //Removes it
                     i--;
@@ -60,13 +84,19 @@
         return {progress: progress, closed: closed};
     };
 
+    var checkOneSprint = function checkOneSprint() {
+        return sprint.every(function (issue) {
+            return issue.fields.version.name === version;
+        });
+    };
+
     // Get the timestamps of each day of the sprint
     var getDays = function getDays(start, end) {
         var result = [];
         var aux = start;
         result.push(aux);
         while (aux < end) {
-            aux += dayLength;
+            aux += DAY_LENGTH;
             result.push(aux);
         }
         return result;
@@ -141,28 +171,11 @@
         MashupPlatform.wiring.pushEvent("chart-options", JSON.stringify(options));
     };
 
-    var cleanPlot = function cleanPlot() {
+
+    var cleanPlot = function cleanPlot(msg) {
         var options = {};
+        options.title = {text: msg};
         MashupPlatform.wiring.pushEvent("chart-options", JSON.stringify(options));
     };
 
-    MashupPlatform.wiring.registerCallback("issues", function (data) {
-        sprint = data;
-        version = null;
-        if (sprint.length === 0) {
-            cleanPlot();
-            return;
-        }
-
-        version = sprint[0].fields.version.name;
-
-        data.metadata.versions.forEach(function (ver) {
-            if (ver.name === version) {
-                startDate = ver.startDate;
-                endDate = ver.releaseDate;
-            }
-        });
-
-        plot();
-    });
 })();
