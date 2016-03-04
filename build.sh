13cf0974059022c24e373b5c339c59dceaab3c04
Alejandro Rodriguez <alex.mognom@gmail.com>
2016-03-04 09:45:38 +0100

    Can npm install

--- build.sh
+++ build.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+
+if [ "$1" == "install" ]; then
+    command="npm install"
+else
+    command="grunt $1"
+fi
+
 for i in * ; do
     if [ -d "$i" -a -f "$i/Gruntfile.js" ]; then
         cd $i
         cat << EOF
+
 
 #######
 Buiding ${i}
@@ -11,7 +19,7 @@ Buiding ${i}
 
 
 EOF
-        grunt $1
+        $command
         cd ..
     fi
 done
