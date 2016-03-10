cd2f13e7229bfccfdc7010637239b2552526dfab
Alejandro Rodriguez <alex.mognom@gmail.com>
2016-03-10 12:57:16 +0100

    Commit workload and pie chart if data is from the same month

--- workload-chart-generator-operator/src/doc/index.md
+++ workload-chart-generator-operator/src/doc/index.md
@@ -1,16 +1,27 @@
 ## Introduction
 
-The best project ever.
+The Workload Chart Generator operator is a WireCloud operator that provides the ability to plot workload charts of a agile project based on assigned issues or the number of commits made by each author.
 
 ## Settings
+
+- `Chart Title`: The title for the chart.
 
 ## Wiring
 
 ### Input Endpoints
 
+- `Issues`: The input issues
+- `Commits`: The input commits
+
+
 ### Output Endpoints
 
+- `Chart Data Model`: The chart data model to be passed to the Highcharts widget.
+
 ## Usage
+
+Plug in the issues/commits you want analyzed and plug the output to the Highcharts widget.
+You can harvest the issues/commits with the github or gitlab harvesters and filter them using the `set-generic-filter-conditions` operator and the `and-filter-operator`.
 
 ## Reference
 
--- workload-chart-generator-operator/src/js/main.js
+++ workload-chart-generator-operator/src/js/main.js
@@ -15,24 +15,61 @@
 
         MashupPlatform.wiring.registerCallback("issues", function (data) {
             var series = calculateIssueSeries(data);
-            build_column_chart(series.serie, series.months);
+            if (series.months.length === 1) {
+                build_pie_chart(series.serie);
+            } else {
+                build_column_chart(series.serie, series.months, "Issues");
+            }
         });
 
         MashupPlatform.wiring.registerCallback("commits", function (data) {
-            var serie = calculateCommitSeries(data);
-            //build_pie_chart(serie);
+            var series = calculateCommitSeries(data);
+            if (series.months.length === 1) {
+                build_pie_chart(series.serie);
+            } else {
+                build_column_chart(series.serie, series.months, "Commits");
+            }
         });
 
     };
 
+    //Get the series to be plotted from the provided commits
     var calculateCommitSeries = function calculateCommitSeries (commits) {
+        var result = [];
+        var months = [];
+        commits.forEach(function (commit) {
+            var author = commit.author;
+            //If it has no author skip it.
+            if (!author) {
+                return;
+            }
+            var month = commit.month;
 
+            var i = months.indexOf(month);
+            if (i === -1) {
+                months.push(month);
+                i = months.length - 1;
+            }
+            //Count the total commits per month
+            if (result[author]) {
+                if (result[author][i]) {
+                    result[author][i] += 1;
+                } else {
+                    result[author][i] = 1;
+                }
+            } else {
+                // If its the first issue of the author, initialize the array
+                result[author] = [];
+                result[author][i] = 1;
+            }
+        });
+        return {serie: result, months: months};
     };
 
+    //Get the series to be plotted from the provided issues
     var calculateIssueSeries = function calculateIssueSeries (issues) {
         var result = [];
         var months = [];
-        
 
         issues.forEach (function (issue) {
 
@@ -42,13 +79,13 @@
                 return;
             }
             var month = issue.creationDate.substring(0, 7);
-            
+
             var i = months.indexOf(month);
             if (i === -1) {
                 months.push(month);
                 i = months.length - 1;
-            } 
-            
+            }
+            //Count issues
             if (result[assignee]) {
                 if (result[assignee][i]) {
                     result[assignee][i] += 1;
@@ -65,8 +102,8 @@
         return {serie: result, months: months};
     };
 
-    var build_column_chart = function build_column_chart(serie, months) {
-        
+    var build_column_chart = function build_column_chart(serie, months, type) {
+        //Convert it to the highcharts series
         var series = [];
         var keys = Object.keys(serie);
         keys.forEach(function (key, i) {
@@ -85,7 +122,7 @@
             },
             yAxis: {
                 title: {
-                    text: "Issues"
+                    text: type
                 }
             },
             title: {
@@ -100,8 +137,30 @@
         MashupPlatform.wiring.pushEvent("chart-options", JSON.stringify(options));
     };
 
-    var build_pie_chart = function build_pie_chart(serie) {
+    var build_pie_chart = function build_pie_chart(series) {
+        //Wait for data
+        var serie = [];
+        var keys = Object.keys(series);
+        keys.forEach(function (key) {
+            serie.push({name: key, y: series[key][0]});
+        });
 
+        var options = {
+            chart: {
+                type: 'pie'
+            },
+            title: {
+                text: MashupPlatform.prefs.get('title')
+            },
+            legend: {
+                enabled: true
+            },
+            series: [{
+                data: serie
+            }]
+        };
+        //Push the highcharts options
+        MashupPlatform.wiring.pushEvent("chart-options", JSON.stringify(options));
     };
 
     init();
