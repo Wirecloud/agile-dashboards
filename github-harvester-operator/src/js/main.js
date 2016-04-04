69d1f0cdf0252730e280841203bd0a82334be658
Alejandro Rodriguez <alex.mognom@gmail.com>
2016-04-04 10:41:31 +0200

    Harvest issue events

--- github-harvester-operator/src/js/main.js
+++ github-harvester-operator/src/js/main.js
@@ -18,6 +18,11 @@
     var repoName;
     var username;
 
+    var issueData;
+    var requestHeaders;
+
+    var eventsToDo;
+
     //Stablish callbacks
     var init = function init() {
 
@@ -42,7 +47,7 @@
 
     var request_github_info = function request_github_info() {
 
-        var requestHeaders = {
+        requestHeaders = {
             Accept: "application/vnd.github.v3.html+json"
         };
 
@@ -66,17 +71,17 @@
                 requestHeaders: requestHeaders,
                 onSuccess: function (response) {
                     milestones = [];
-                    var issues = normalizeData(JSON.parse(response.responseText));
+                    issueData = normalizeData(JSON.parse(response.responseText));
 
                     //Calculate the sprints
                     guessMilestonesBeginDate ();
 
                     //Add some metadata
-                    issues.metadata = {};
-                    issues.metadata.versions = milestones;
-                    issues.metadata.type = "list";
-                    issues.metadata.tag = "Issue";
-                    issues.metadata.verbose = "Github issues";
+                    issueData.metadata = {};
+                    issueData.metadata.versions = milestones;
+                    issueData.metadata.type = "list";
+                    issueData.metadata.tag = "Issue";
+                    issueData.metadata.verbose = "Github issues";
                     //filter metadata
                     var filters = [];
                     filters.push({name: "Sprints", base: "metadata.versions", property: "name", display: "name", compare: "versions", type: "some"});
@@ -85,12 +90,14 @@
                     filters.push({name: "Label", property: "labels", display: "labels", type: "some"});
                     filters.push({name: "Issue Key", property: "key", display: "key"});
                     filters.push({name: "Creation month", property: "month", display: "month"});
-                    issues.metadata.filters = filters;
+                    issueData.metadata.filters = filters;
 
-                    MashupPlatform.wiring.pushEvent("issue-list", issues);
+                    MashupPlatform.wiring.pushEvent("issue-list", issueData);
+
+                    //Harvest extra data such as event data
+                    harvestEvents();
                 }
             });
-
         }
 
         if (MashupPlatform.operator.outputs['commit-list'].connected) {
@@ -123,6 +130,45 @@
                 }
             });
         }
+    };
+
+    //Harvest event data
+    var harvestEvents = function harvestEvents() {
+        eventsToDo = issueData.length;
+        issueData.forEach(function (issue) {
+            harvestIssueEvents(issue);
+        });
+    };
+
+    var harvestIssueEvents = function harvestIssueEvents (issue) {
+
+        MashupPlatform.http.makeRequest("https://api.github.com/repos/" + username + "/" + repoName + "/issues/" + issue.key.substring(1, issue.key.length) + "/events", {
+            method: 'GET',
+            supportsAccessControl: true,
+            requestHeaders: requestHeaders,
+            onSuccess: function (response) {
+                var data = JSON.parse(response.responseText);
+
+                data.forEach(function (event) {
+                    if (event.event === "milestoned") {
+                        if (issue.versions.indexOf(event.milestone.title) === -1) {
+                            issue.versions.push(event.milestone.title);
+                            addMilestones (event.milestone.title, event.milestone.due_on);
+                        }
+                    }
+                });
+            },
+            onComplete: function (response) {
+                if (--eventsToDo === 0) {
+                    //Update repository milestones, just in case any new milestone was found
+                    guessMilestonesBeginDate();
+                    issueData.metadata.versions = milestones;
+
+                    //Push the new data
+                    MashupPlatform.wiring.pushEvent("issue-list", issueData);
+                }
+            }
+        });
     };
 
     //Removes useless JSON data and gives a normalized format
