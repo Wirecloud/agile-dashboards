2933ab237b08eec1aeacad11f82529192d1ad1a4
Alejandro Rodriguez <alex.mognom@gmail.com>
2016-05-12 11:01:06 +0200

    Added tests

--- workload-chart-generator-operator/src/test/js/WorkloadChartGeneratorSpec.js
+++ workload-chart-generator-operator/src/test/js/WorkloadChartGeneratorSpec.js
@@ -1,17 +1,6 @@
-/*global $, MashupPlatform, MockMP, WorkloadChartGenerator, beforeAll, afterAll, beforeEach*/
+/*global describe, it ,expect , MashupPlatform, MockMP, issuesCallback, commitsCallback, beforeAll, afterAll, beforeEach*/
 (function () {
     "use strict";
-
-    //jasmine.getFixtures().fixturesPath = 'src/test/fixtures/';
-
-    var dependencyList = [
-        'script',
-        'div',
-    ];
-
-    var clearDocument = function clearDocument() {
-        $('body > *:not(' + dependencyList.join(', ') + ')').remove();
-    };
 
     describe("Test WorkloadChartGenerator", function () {
         var widget;
@@ -21,11 +10,76 @@
 
         beforeEach(function () {
             MashupPlatform.reset();
-            //widget = new WorkloadChartGenerator();
         });
 
         it("Dummy test", function () {
             expect(true).toBeTruthy();
+        });
+
+        describe("Should calculate issue workload", function () {
+            it("Should calculate pie chart when only one month of data", function () {
+                var issues = [
+                    {assignee: "juan", creationDate: "month1"},
+                    {assignee: "juan", creationDate: "month1"},
+                    {assignee: "paco", creationDate: "month1"},
+                    {creationDate: "month1"}
+                ];
+                issuesCallback (issues);
+                var call = MashupPlatform.wiring.pushEvent.calls.first().args;
+                expect(call[0]).toBe("chart-options");
+                var args = JSON.parse(call[1]);
+
+                expect(args.series[0].data).toEqual([{name: "juan", y:2}, {name: "paco", y:1}]);
+            });
+
+            it("Should calculate column chart when more than one month of data", function () {
+                var issues = [
+                    {assignee: "juan", creationDate: "month1"},
+                    {assignee: "juan", creationDate: "month1"},
+                    {assignee: "paco", creationDate: "month1"},
+                    {assignee: "paco", creationDate: "month2"},
+                    {assignee: "paco", creationDate: "month2"},
+                ];
+                issuesCallback (issues);
+                var call = MashupPlatform.wiring.pushEvent.calls.first().args;
+                expect(call[0]).toBe("chart-options");
+                var args = JSON.parse(call[1]);
+
+                expect(args.series).toEqual([{name:"juan", data: [2]}, {name: "paco", data: [1, 2]}]);
+            });
+        });
+
+        describe("Should calculate commit workload", function ()  {
+            it("Should calculate pie chart when only one month of data", function () {
+                var commits = [
+                    {author: "juan", month: "month1"},
+                    {author: "paco", month: "month1"},
+                    {author: "juan", month: "month1"},
+                    {month: "month1"}
+                ];
+
+                commitsCallback(commits);
+                var call = MashupPlatform.wiring.pushEvent.calls.first().args;
+                expect(call[0]).toBe("chart-options");
+                var args = JSON.parse(call[1]);
+                expect(args.series[0].data).toEqual([{name:"juan", y: 2}, {name:"paco", y: 1}]);
+            });
+
+            it("Should calculate column chart when more than one month of data", function () {
+                var commits = [
+                    {author: "juan", month: "month1"},
+                    {author: "paco", month: "month1"},
+                    {author: "juan", month: "month1"},
+                    {author: "paco", month: "month2"},
+                    {author: "paco", month: "month2"}
+                ];
+
+                commitsCallback(commits);
+                var call = MashupPlatform.wiring.pushEvent.calls.first().args;
+                expect(call[0]).toBe("chart-options");
+                var args = JSON.parse(call[1]);
+                expect(args.series).toEqual([{name:"juan", data: [2]}, {name: "paco", data: [1, 2]}]);
+            });
         });
 
     });
