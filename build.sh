8a2316e5a5ea5c7a3e694d379d01862a1322bb25
Alejandro Rodriguez <alex.rodriguez@conwet.com>
2016-07-20 11:17:42 +0200

    Improved build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+echo $0
+#Go to the right directory
+cd $(dirname $0)
 
 if [ "$1" == "install" ]; then
     command="npm install"
 else
     command="grunt $1"
+    rm dist/* 2> /dev/null
 fi
 
 for i in * ; do
@@ -19,7 +23,12 @@ Buiding ${i}
 
 
 EOF
+        rm dist/* 2> /dev/null
         $command
+        if [ "$?" -ne "0" ]; then
+        	exit 1
+        fi
+        cp dist/* ../dist/
         cd ..
     fi
 done
